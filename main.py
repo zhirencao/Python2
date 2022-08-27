05e1c600301ca19ec7e9e6c1d5b7959c6ac02408
zhirencao <277645743@qq.com>
2022-08-27 09:26:11 +0800

    8.27

--- main.py
+++ main.py
@@ -1,34 +1,18 @@
+import sys
 
-import cv2
-
-
-def face_rec():
-    # 加载视频
-    cameraCapture = cv2.VideoCapture('D:/3.mp4')
-    # cv2级联分类器CascadeClassifier,xml文件为训练数据
-    face_cascade = cv2.CascadeClassifier('D:/Program Files/opencv/sources/data/haarcascades/haarcascade_frontalface_default.xml')
-    # 读取数据
-    success, frame = cameraCapture.read()
-    while success and cv2.waitKey(1) == -1:
-        # 读取数据
-        ret, img = cameraCapture.read()
-        # 进行人脸检测
-        faces = face_cascade.detectMultiScale(img, 1.3, 5)
-        # 绘制矩形框
-        for (x, y, w, h) in faces:
-            img = cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
-        # 设置显示窗口
-        cv2.namedWindow('camera', 0)
-        cv2.resizeWindow('camera', 480, 640)
-        # 显示处理后的视频
-        cv2.imshow('camera', img)
-        # 读取数据
-        success, frame = cameraCapture.read()
-    # 释放视频
-    cameraCapture.release()
-    # 释放所有窗口
-    cv2.destroyAllWindows()
-
+from PyQt5.QtGui import QIcon
+from PyQt5.QtWidgets import QApplication, QWidget
 
 if __name__ == '__main__':
-    face_rec()
+    app = QApplication(sys.argv)
+
+    # 创建一个QWidget
+    w = QWidget()
+    # 设置标题
+    w.setWindowTitle("看看我图标帅吗")
+    # 设置图标
+    w.setWindowIcon(QIcon('D:/8.png'))
+    # 显示QWidget
+    w.show()
+
+    app.exec()
